48d12fd1edaa6ee98fdd942067e36a98a60ff4f8
Rick <{ID}+{username}@users.noreply.github.com>
2019-10-19 23:37:43 +0900

    ADD: Show Products in Cards

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from './Layout';
+import {getProducts} from './apiCore';
+import Card from './Card';
 
-const Home = () => (
-  <Layout title="Home" description="Ecommerce APP" className children>
-    ...
-  </Layout>
+const Home = () => {
+  const [productsBySold, setProductsBySold] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
 
-);
+  const loadProductsBySold = () => {
+    getProducts('sold').then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySold(data);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts('createdAt').then(data => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySold();
+  }, []);
+
+  return (
+    <Layout title="Home" description="Ecommerce APP" className="container-fluid" children>
+      <h2 className="mb-4">New Arrivals</h2>
+
+      <div className="row">
+        {productsByArrival.map((prod, idx) => (
+          <Card key={idx} product={prod} />
+        ))}
+      </div>
+
+      <h2 className="mb-4">Best Sellers</h2>
+      <div className="row">
+        {productsBySold.map((prod, idx) => (
+          <Card key={idx} product={prod} />
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
 export default Home;
