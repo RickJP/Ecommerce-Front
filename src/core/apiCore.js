3c77bfe0ae43c25fb6cc912ac29878f2ec06aa2b
Rick <{ID}+{username}@users.noreply.github.com>
2019-10-25 09:12:16 +0900

    ADD: Braintree DropIn for payment options

--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -76,3 +76,18 @@ export const list = params => {
   })
   .catch(err => console.log(err));
 };
+
+export const getBraintreeClientToken = (userId, token) => {
+  return fetch(`${API}/braintree/getToken/${userId}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+  })
+  .then(res => {
+    return res.json();
+  })
+  .catch(err => console.log(err));
+};
