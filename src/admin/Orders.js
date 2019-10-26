926305580c6c4b4eb78d7fd8a51c1121c9dc615b
Rick <{ID}+{username}@users.noreply.github.com>
2019-10-26 13:21:00 +0900

    ADD: Allow updates for status values for each order

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react';
 import Layout from '../core/Layout';
 import {isAuthenticated} from '../auth';
 import {Link} from 'react-router-dom';
-import {listOrders} from './apiAdmin';
+import {listOrders, getStatusValues} from './apiAdmin';
 import moment from 'moment';
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [statusValues, setStatusValues] = useState([]);
 
   const {user, token} = isAuthenticated();
 
@@ -20,8 +21,19 @@ const Orders = () => {
     });
   };
 
+  const loadStatusValues = () => {
+    getStatusValues(user._id, token).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setStatusValues(data);
+      }
+    });
+  };
+
   useEffect(() => {
     loadOrders();
+    loadStatusValues();
   }, []);
 
   const showOrdersLength = () => {
@@ -34,14 +46,35 @@ const Orders = () => {
     }
   };
 
-const  showInput = (key, value) => (
-  <div className="input-group mb-2 mr-sm-2">
-    <div className="input-group-prepend">
-      <div className="input-group-text">{key}</div>
+  const showInput = (key, value) => (
+    <div className="input-group mb-2 mr-sm-2">
+      <div className="input-group-prepend">
+        <div className="input-group-text">{key}</div>
+      </div>
+      <input type="text" value={value} className="form-control" readOnly />
     </div>
-    <input type="text" value={value} className="form-control" readonly />
-  </div>
-)
+  );
+
+  const handleStatusChange = (e, orderId) => {
+    console.log("update order status");
+  }
+
+  const showStatus = o => (
+    <div className="form-group">
+      <h3 className="mark mb-4">Status: {o.status} </h3>
+      <select
+        className="form-control"
+        onChange={(e) => handleStatusChange(e, o._id)}
+      >
+        <option>Update Status</option>
+        {statusValues.map((status, idx) => (
+          <option key={idx} value={status}>
+            {status}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 
   return (
     <Layout
@@ -63,21 +96,17 @@ const  showInput = (key, value) => (
                   <h2 className="mb-5">
                     <span className="bg-primary">Order ID: {o._id}</span>
                   </h2>
-                  <li className="list-group-item">{o.status}</li>
+                  <li className="list-group-item">{showStatus(o)}</li>
                   <li className="list-group-item">
                     Transaction ID: {o.transaction_id}
                   </li>
+                  <li className="list-group-item">Amount: $ {o.amount}</li>
+                  <li className="list-group-item">Ordered by: {o.user.name}</li>
                   <li className="list-group-item">
-                    Amount: $ {o.amount}
+                    Ordered on: {moment(o.createdAt).fromNow()}
                   </li>
                   <li className="list-group-item">
-                    Ordered by: {o.user.name}
-                  </li>
-                  <li className="list-group-item">
-                   Ordered on: {moment(o.createdAt).fromNow()}
-                  </li>
-                  <li className="list-group-item">
-                   Delivery Address: {o.address}
+                    Delivery Address: {o.address}
                   </li>
                 </ul>
 
@@ -86,7 +115,11 @@ const  showInput = (key, value) => (
                 </h3>
 
                 {o.products.map((p, pIdx) => (
-                  <div className="mb-4" key={pIdx} style={{padding: '20px', border: '1px solid indigo'}}>
+                  <div
+                    className="mb-4"
+                    key={pIdx}
+                    style={{padding: '20px', border: '1px solid indigo'}}
+                  >
                     {showInput('Product name', p.name)}
                     {showInput('Product price', p.price)}
                     {showInput('Product total', p.count)}
