8cd1736f785e4509f04e4729fd721cdc617f327a
Rick <{ID}+{username}@users.noreply.github.com>
2019-10-26 21:07:25 +0900

    ADD: Get user info for profile page

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from '../core/Layout';
 import {isAuthenticated} from '../auth';
 import {Link} from 'react-router-dom';
+import {read, update, updateUser} from './apiUser.js';
 
-const Profile = () => {
-  return (<h2>Profile</h2>)
+const Profile = ({ match }) => {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: false,
+    success: false,
+  });
+
+  const {name, email, password, error, success} = values;
+
+  const {token} = isAuthenticated();
+
+  const init = (userId) => {
+    //console.log(userId);
+    read(userId, token).then(data => {
+      if (data.error) {
+        setValues({...values, error: true})
+      } else {
+        setValues({...values, name:data.name, email:data.email})
+      }
+    })
+  }
+
+  useEffect(() => {
+    init(match.params.userId);
+  },[])
+
+  return (
+    <Layout title="Profile" description="Update your profile" className="container-fluid">
+      <h2 className="mb-4">Profile Update</h2>
+      {JSON.stringify(values)}
+      
+    </Layout>
+  );
 }
 
 export default Profile;
